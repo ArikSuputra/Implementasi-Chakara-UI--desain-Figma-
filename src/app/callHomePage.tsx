c521fab0b6d6c86c19db544bd0898cd3dce23517
AS <ariksuputra13@gmail.com>
2024-12-21 18:10:14 +0800

    4.4

--- src/app/callHomePage.tsx
+++ src/app/callHomePage.tsx
@@ -1,47 +1,115 @@
-'use client';
+"use client";
 import { Box, Flex, Radio, RadioGroup, Stack, Text } from "@chakra-ui/react";
 import Image from "next/image";
-import CardBox from './components/dekstop/cardbox';
+import CardBox from "./components/dekstop/cardbox";
+import CardBox1 from "./components/mobile/cardbox";
 import { useState } from "react";
+import { BsJustify } from "react-icons/bs";
 
 export default function Home() {
   const [value, setValue] = useState("1");
 
   return (
-    <Flex
-      direction="column"
-      justify="center"
-      align="center"
-      height="100vh"
-      width="100%"
-      position="relative"
-      overflow="hidden"
-    >
-      {/* Background Image */}
-      <Box position="absolute" top="0" left="-45px" right="0" bottom="0" zIndex="-1">
-        <Image 
-          src="/images/BackGround.png"
-          style={{ objectFit: "cover" }}
-          alt="background"
-          fill
-        />
-      </Box>
+    <>
+      <Flex
+        direction="column"
+        justify="center"
+        align="center"
+        height="100vh"
+        width="100%"
+        position="relative"
+        overflow="hidden"
+      >
+        {/* Background Image */}
+        <Box
+          position="absolute"
+          top="0"
+          left="-45px"
+          right="0"
+          bottom="0"
+          zIndex="-1"
+        >
+          <Image
+            src="/images/BackGround.png"
+            style={{ objectFit: "cover" }}
+            alt="background"
+            fill
+          />
+        </Box>
+
+        {/* Main Content */}
+
+        {/* Radio Group at Bottom */}
+        {/* Radio Group at Bottom */}
+        <Box
+          position="absolute"
+          bottom="30px"
+          left="0"
+          right="0"
+        >
+          <Flex justify="center" align="center" display={{ base: "none", lg: "flex" }}>
+            <RadioGroup onChange={setValue} value={value}>
+              <Stack direction="row" spacing={8}>
+                {["1", "2", "3", "4"].map((val) => (
+                  <Radio
+                    key={val}
+                    value={val}
+                    sx={{
+                      // Remove Chakra's default focus outline
+                      "&:focus, &:focus-visible": {
+                        boxShadow: "none",
+                      },
+
+                      // Outer circle styles
+                      "& .chakra-radio__control": {
+                        width: "20px",
+                        height: "20px",
+                        borderWidth: "2px",
+                        borderColor: "white", // Outer circle color
+                        backgroundColor: "transparent", // Transparent background
+                        borderRadius: "50%",
+                        _checked: {
+                          backgroundColor: "transparent", // No blue background
+                          borderColor: "white", // Outer circle stays white
+                        },
+                      },
+
+                      // Inner dot styles
+                      "& .chakra-radio__control::before": {
+                        content: '""',
+                        display: "block",
+                        width: "10px",
+                        height: "10px",
+                        backgroundColor: value === val ? "white" : "gray.500", // White when active, gray when inactive
+                        borderRadius: "50%",
+                        position: "relative",
+                        margin: "auto",
+                      },
+                    }}
+                  />
+                ))}
+              </Stack>
+            </RadioGroup>
+          </Flex>
+        </Box>
+      </Flex>
       <Box
         position="absolute"
-        bottom="50px"   // Distance from bottom
-        right="50px"     // Distance from left
+        bottom="50px" // Distance from bottom
+        right="50px" // Distance from left
         zIndex="3"
-        display={{ base: "none", lg: "flex" }} 
+        display={{ base: "none", lg: "flex" }}
       >
         <CardBox />
       </Box>
 
-      {/* Main Content */}
-
-    {/* Radio Group at Bottom */}
-     {/* Radio Group at Bottom */}
-      <Box position="absolute" bottom="30px" left="0" right="0">
-        <Flex justify="center" align="center">
+      <Box
+        position="absolute"
+        bottom="250px"
+        left="0"
+        right="0"
+      >
+        <Flex justify="center" align="center" display={{ base: "flex", lg: "none" }}>
           <RadioGroup onChange={setValue} value={value}>
             <Stack direction="row" spacing={8}>
               {["1", "2", "3", "4"].map((val) => (
@@ -85,7 +153,18 @@ export default function Home() {
             </Stack>
           </RadioGroup>
         </Flex>
-      </Box>    
-      </Flex>
+      </Box>
+
+      <Box
+        position="absolute"
+        bottom="50px" // Distance from bottom
+        width="100%"
+        height="auto"
+        zIndex="-1"
+        display={{ base: "flex", lg: "none" }}
+      >
+        <CardBox1 />
+      </Box>
+    </>
   );
 }
---
+++ src/app/components/mobile/cardbox.tsx
@@ -0,0 +1,46 @@
+import { Box, Heading, Text, Button, Flex } from "@chakra-ui/react";
+import { DM_Serif_Display , Quicksand } from '@next/font/google';
+import {GoArrowRight} from "react-icons/go";
+const dmSerifDisplay = DM_Serif_Display({
+    weight: '400', 
+    subsets: ['latin'],
+});
+
+const quicksand = Quicksand({
+    weight: '400', 
+    subsets: ['latin'],
+});
+
+const CardBox = () => {
+  return (
+    <Box
+      bg="#E1D5CECC"
+      p="30px"
+      width="100%"
+      height="200"
+      display="flex"
+      flexDirection="column"
+      gap={2}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Heading size="md" color="black" fontFamily={dmSerifDisplay.style.fontFamily}>
+        The Rock and Roll Hall
+      </Heading>
+      <Text fontSize="sm" color="gray.600" fontFamily={quicksand.style.fontFamily}>
+        <b>Client:</b> PT Devkoma Development <br />
+        <b>Design:</b> Kapinala Studio
+      </Text>
+      <Flex direction="row">
+      <Text fontFamily={quicksand.style.fontFamily}>
+        See details 
+      </Text>
+      <Flex pt="5px" pl="7px">
+        <GoArrowRight/>
+      </Flex>
+      </Flex>
+    </Box>
+  );
+};
+
+export default CardBox;
